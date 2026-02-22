fa8bf74b12261797dd23cd571b1fa17c1543533d
PranavGore1221 <pranav.gore@vmvinfotech.com>
2026-02-22 12:39:35 +0530

    Initial commit - AI CI/CD Spring Boot project

---
+++ src/main/java/com/pranav/ai_cicd_demo/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.pranav.ai_cicd_demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from AI-enabled CI/CD Pipeline!";
+    }
+}
---
+++ src/test/java/com/pranav/ai_cicd_demo/AiCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.ai_cicd_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
